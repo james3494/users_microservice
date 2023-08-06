2067aff0fcf85442dd86e69d0520c41901a5cce2
james7494 <james.mabon@btinternet.com>
2023-08-06 08:31:02 +0100

    update error handling for entity

--- entities/user.js
+++ entities/user.js
@@ -1,7 +1,4 @@
-/* TODO
-Create add / remove friend functionality.
-testing
-*/
+
 
 module.exports = {
   buildMakeUser ({ Id, hashMachine, throwError, isValidEmail, isValidName }) {
@@ -20,54 +17,54 @@ module.exports = {
     } = {}) {
 
       if (!Id.isValidId(_id)) {
-       throwError('User must have a valid id.', 400);
+       throwError('User must have a valid id.', "user-invalid-id", 400);
       }
       if (!isValidEmail(email)) {
-       throwError('User must have a valid email.', 400);
+       throwError('User must have a valid email.', "user-invalid-email", 400);
       }
       if (!isValidName(lastName)) {
-       throwError('User must have a valid lastName.', 400);
+       throwError('User must have a valid lastName.', "user-invalid-lastName", 400);
       }
       if (!isValidName(firstName)) {
-       throwError('User must have a valid firstName.', 400);
+       throwError('User must have a valid firstName.', "user-invalid-firstName", 400);
       }
       if (!hashMachine.isValidHash(hash)) {
-       throwError('User must have a valid hash.', 400);
+       throwError('User must have a valid hash.', "user-invalid-hash", 400);
       }
       if (!hashMachine.isValidSalt(salt)) {
-       throwError('User must have a valid salt.', 400);
+       throwError('User must have a valid salt.', "user-invalid-salt", 400);
       }
       if (typeof disabled !== 'boolean') {
-        throwError('Disabled must be a boolean.', 400);
+        throwError('Disabled must be a boolean.', "user-invalid-disabled", 400);
       }
       if (typeof modifiedOn !== 'number' || modifiedOn > Date.now()) {
-        throwError('modifiedOn must be a number and in the past.', 400);
+        throwError('modifiedOn must be a number and in the past.', "user-invalid-modifiedOn", 400);
       }
       if (typeof createdOn !== 'number' || createdOn > Date.now()) {
-        throwError('createdOn must be a number and in the past.', 400);
+        throwError('createdOn must be a number and in the past.', "user-invalid-createdOn", 400);
       }
       // groups check
       if (typeof groups !== 'object' || !Array.isArray(groups)) {
-       throwError('User groups must be an array.', 400);
+       throwError('User groups must be an array.', "user-invalid-groups", 400);
       }
       if ((new Set(groups)).size !== groups.length) {
-       throwError('User must not have repeated groups.', 400);
+       throwError('User must not have repeated groups.', "user-invalid-groups", 400);
       }
       const allowedGroups = ['usersAdmin', 'huntedAdmin', 'superAdmin'];
-      if (!groups.every(group => allowedGroups.includes(group))) {
-       throwError('All user groups must be one of [' + allowedGroups.reduce((string, group) => string + ' ' + group) + ']', 400);
+        if (!groups.every(group => allowedGroups.includes(group))) {
+       throwError('All user groups must be one of [' + allowedGroups.reduce((string, group) => string + ' ' + group) + ']', "user-invalid-groups", 400);
       }
 
 
       // friends check
       if (typeof friends !== 'object' || !Array.isArray(friends)) {
-       throwError('User friends must be an array.', 400);
+       throwError('User friends must be an array.', "user-invalid-friends", 400);
       }
       if ((new Set(friends)).size !== friends.length) {
-       throwError('User must not have repeated friends.', 400);
+       throwError('User must not have repeated friends.', "user-invalid-friends", 400);
       }
       if (!friends.every(friend => Id.isValidId(friend))) {
-       throwError('All user friends must have a valid ID.', 400);
+       throwError('All user friends must have a valid ID.', "user-invalid-friends", 400);
       }
 
       return Object.freeze({
@@ -84,7 +81,7 @@ module.exports = {
         isDisabled: () => disabled,
         resetPassword: (password) => {
          if(!hashMachine.isValidPassword(password)) {
-           throwError('Please choose a valid password.', 400);
+           throwError('Invalid password.', "user-invalid-password", 400, "Passwords must be between 8 and 16 characters long.");
          }
          hash = hashMachine.hash(password, salt);
         },
--- todo.md
+++ todo.md
@@ -1,9 +1,21 @@
  - rewrite errors to return informative and standardised messages
+    - controllers
+    - move catchErrors to expressCallback
+    - make errorHandling not use a standardised format tied to expressCallback
+
  - create a 'getFullUser' endpoint which is accessible to admins
  - write tests!
  - write documentation in order to clear up exactly what I'm after.
+
  - salt and hash validation in /entities/hashMachine.js
  - way of sharing validation between front and back end
+ - move out validation to a seperate module and have functions return something more useful
+
+ - change sessions to return just a status then another endpoint to get info (more of a todo for gateway)
+
  - rename post sessions to auth
  - add / remove friend functionality
  - delete user functionality? (would be useful for testing)
+ - create a ping endpoint
+
+ - look into whether the correct headers are being used
